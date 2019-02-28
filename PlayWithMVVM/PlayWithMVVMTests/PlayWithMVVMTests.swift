49d6aa92e9dd833ebf2b70ead554a19ed2610a96
VIET Do Hoang <viet.dohoang>
2019-02-28 09:13:46 +0700

    Init project

---
+++ PlayWithMVVM/PlayWithMVVMTests/PlayWithMVVMTests.swift
@@ -0,0 +1,34 @@
+//
+//  PlayWithMVVMTests.swift
+//  PlayWithMVVMTests
+//
+//  Created by Do Hoang Viet on 2/28/19.
+//  Copyright © 2019 Do Hoang Viet. All rights reserved.
+//
+
+import XCTest
+@testable import PlayWithMVVM
+
+class PlayWithMVVMTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
